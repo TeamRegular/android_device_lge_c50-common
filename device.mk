c27f46664b9fc573aac41b949e4e5ae8bfe4e370
C457 <android.c357@gmail.com>
2015-07-13 09:22:57 -0500

    ms345: initial camerawrapper
    * kanged from v4xx

--- device.mk
+++ device.mk
@@ -70,6 +70,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8916
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
